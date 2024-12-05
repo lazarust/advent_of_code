976e346eaf8808e61f89884e3f3d08ecf4f6f65e
Thomas Lazarus <lazarustda@gmail.com>
2024-12-04 21:42:36 -0600

    Day 3 Part 2 in Rust

--- rust/src/day3/functions.rs
+++ rust/src/day3/functions.rs
@@ -5,22 +5,30 @@ pub fn challenge() -> Result<(), Box<dyn std::error::Error>> {
     let contents =
         fs::read_to_string("../inputs/day3.txt").expect("Should have been able to read the file");
 
-    let re = Regex::new(r"(mul\(\d{1,},\d{1,}\))").unwrap();
+    let re = Regex::new(r"(mul\(\d{1,},\d{1,}\)|do\(\)|don't\(\))").unwrap();
     let mut results = vec![];
     for (_, [data]) in re.captures_iter(&contents).map(|c| c.extract()) {
+        println!("{}", data);
         results.push(data);
     }
 
+    let mut enabled = true;
     let mut running_total = 0;
     for result in results {
         println!("{}", result);
-        let re2 = Regex::new(r"(\d{1,})").unwrap();
-        let mut results2 = vec![];
-        for (_, [data2]) in re2.captures_iter(result).map(|d| d.extract()) {
-            println!("{}", data2);
-            results2.push(data2.parse::<u64>()?);
+        if result == "don't()" {
+            enabled = false;
+        } else if result == "do()" {
+            enabled = true;
+        } else if enabled == true {
+            let re2 = Regex::new(r"(\d{1,})").unwrap();
+            let mut results2 = vec![];
+            for (_, [data2]) in re2.captures_iter(result).map(|d| d.extract()) {
+                println!("{}", data2);
+                results2.push(data2.parse::<u64>()?);
+            }
+            running_total += results2[0] * results2[1];
         }
-        running_total += results2[0] * results2[1];
     }
 
     println!("Total: {}", running_total);
