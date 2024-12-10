a1b1860b547ead17533f82468377c0e9a978e6f9
Thomas Lazarus <lazarustda@gmail.com>
2024-12-09 21:49:39 -0600

    Day 7 Part 1 in Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,11 +2,12 @@
 // mod day2;
 // mod day3;
 // mod day5;
-mod day6;
+// mod day6;
+mod day7;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Call the challenge function
     // Replace with the given day
-    let _ = day6::challenge();
+    let _ = day7::challenge();
     Ok(())
 }
