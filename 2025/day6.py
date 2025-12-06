2f565d1772445e1e89f71bbe511ac0207857c330
Thomas Lazarus <lazarustda@gmail.com>
2025-12-06 17:40:08 -0600

    Day 6 Part 2

--- 2025/day6.py
+++ 2025/day6.py
@@ -7,43 +7,43 @@
 def main() -> None:
     with open("./inputs/day6.txt") as f:
         # with open("./inputs/test_day6.txt") as f:
-        raw_data = f.read().strip()
+        raw_data = f.read().rstrip("\n")
 
     rows = raw_data.splitlines()
     problems = []
     ops = rows[-1]
-    for row in rows[:-1]:
-        col = 0
-        split_row = row.strip().split(" ")
-        for num in split_row:
-            if num != "":
-                if col <= len(problems) - 1 and isinstance(problems[col], list):
-                    problems[col].append(int(num))
+
+    op_locations = []
+    for i, op in enumerate(ops):
+        if op != " ":
+            op_locations.append(i)
+
+    for i, location in enumerate(op_locations):
+        problem = []
+        for row in rows[:-1]:
+            for j in range(
+                location,
+                op_locations[i + 1] - 1 if i < len(op_locations) - 1 else len(row),
+            ):
+                number = row[j]
+                if j - location < len(problem):
+                    problem[j - location] += number
                 else:
-                    problems.append([int(num)])
+                    problem.append(number)
 
-                col += 1
-
-    op_locations = 0
-    for op in ops.strip().split(" "):
-        if op != "":
-            problems[op_locations].append(op)
-            op_locations += 1
+        problems.append(problem)
 
     running_sum = 0
-    for problem in problems:
-        problem.reverse()
-        op = problem[0]
-        problem.pop(0)
-        temp_answer = problem[0]
-        problem.pop(0)
-        for num in problem:
+    for i, op_loc in enumerate(op_locations):
+        op = ops[op_loc]
+        temp_sum = int(problems[i][0])
+        for num in problems[i][1:]:
             if op == "*":
-                temp_answer *= num
+                temp_sum *= int(num)
             else:
-                temp_answer += num
+                temp_sum += int(num)
 
-        running_sum += temp_answer
+        running_sum += temp_sum
 
     print(running_sum)
 
